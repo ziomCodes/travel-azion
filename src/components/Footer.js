e22e9971c767f84c9812325d0fd6d3373e8ae67d
ziomCodes <milladaiz64@gmail.com>
2024-11-12 22:05:21 +0300

    adding footer for the pages

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+
+// Footer.js
+import React from 'react';
+import './styles/App.css';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="social-media">
+                <a href="https://www.facebook.com/profile.php?id=100070185862366" className="fa fa-facebook"></a>
+                <a href="https://www.linkedin.com/in/milion-ziom-code-070b93238" className="fa fa-linkedin"></a>
+                <a href="https://www.instagram.com/ziomcode" className="fa fa-instagram"></a>
+            </div>
+            <p> 2024 AzionTravels</p>
+        </footer>
+    );
+}
+
+export default Footer;
